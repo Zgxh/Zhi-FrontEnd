5be667f7c35f37014d706a50f4a6c684425a5ecc
Zgxh <1227814546@qq.com>
2021-01-05 23:52:05 +0800

    update icons etc

--- src/views/common/index.vue
+++ src/views/common/index.vue
@@ -1,50 +1,86 @@
 <template>
   <div style="width: 100%">
     <el-container>
-      <el-header>
+      <el-header style="padding: 0 0">
         <el-menu
-          :default-active="activeIndex2"
+          :default-active="1"
           class="el-menu-demo"
           mode="horizontal"
           @select="handleSelect"
-          background-color="#0099CC"
+          background-color="#606266"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-menu-item index="1">首页</el-menu-item>
-          <el-menu-item index="2"
-            ><a href="https://www.ele.me" target="_blank"
-              >订单管理</a
-            ></el-menu-item
+          <el-menu-item index="1" style="margin-left: 100px"> 首页 </el-menu-item>
+
+          <el-menu-item index="2">
+            <a href="https://www.ele.me" target="_blank"> 订单管理 </a>
+          </el-menu-item>
+
+          <el-menu-item index="5" > 会员中心 </el-menu-item>
+
+          <el-submenu
+            index="3"
+            style="float: right; margin-right: 20px"
+            v-if="isLogin"
           >
-          <el-submenu index="3" style="float: right" v-if="isLogin">
-            <template slot="title">{{ username }}</template>
-            <el-menu-item index="3-1">我的信息</el-menu-item>
-            <el-menu-item index="3-2">我的文章</el-menu-item>
-            <el-menu-item index="3-3">我的回答</el-menu-item>
-            <el-menu-item index="3-4">退出登录</el-menu-item>
+            <template slot="title">
+              <!-- 展示用户名 + 头像 -->
+              <el-avatar :src="userInfo.avatar"> </el-avatar>
+              &nbsp; {{ userInfo.username }}
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-user-solid"></i> 我的信息
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-question"></i> 我的文章
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-s-opportunity"></i> 我的回答
+            </el-menu-item>
+            <el-menu-item index="3-4">
+              <i class="el-icon-error"></i> 退出登录
+            </el-menu-item>
           </el-submenu>
           <el-menu-item
             index="1"
             v-if="!isLogin"
             style="float: right"
             @click="turnToLogin()"
-            >请登录</el-menu-item
           >
-          <!-- <el-menu-item index="3" style="float: right">消息中心</el-menu-item> -->
-          <el-submenu index="2" style="float: right">
-            <template slot="title">消息中心</template>
-            <el-menu-item index="2-1">
+            请登录
+          </el-menu-item>
+
+          <el-submenu index="4" style="float: right" popper-class="" :disabled="!isLogin">
+            <template slot="title">
+              <i class="el-icon-message"></i> 消息中心
+            </template>
+            <el-menu-item index="4-1">
               最新评论
-              <!-- <el-badge class="mark" :value="12" :max="9" /> -->
+              <el-badge
+                class="mark"
+                :value="messageInfo.newCommentsCount"
+                :max="9"
+                v-if="messageInfo.newCommentsCount > 0"
+              />
             </el-menu-item>
-            <el-menu-item index="2-2">
-              最新回复
-              <!-- <el-badge class="mark" :value="12" :max="9" /> -->
+            <el-menu-item index="4-2">
+              最新回答
+              <el-badge
+                class="mark"
+                :value="messageInfo.newAnswersCount"
+                :max="9"
+                v-if="messageInfo.newAnswersCount > 0"
+              />
             </el-menu-item>
-            <el-menu-item index="2-3">
+            <el-menu-item index="4-3">
               新的关注
-              <!-- <el-badge class="mark" :value="12" :max="9" /> -->
+              <el-badge
+                class="mark"
+                :value="messageInfo.newFollowersCount"
+                :max="9"
+                v-if="messageInfo.newFollowersCount > 0"
+              />
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -59,16 +95,26 @@ export default {
   name: "Index",
   data() {
     return {
-      activeIndex: "1",
-      activeIndex2: "1",
-      username: "",
-      isLogin: true,
-      articles: [],
+      // 保存用户信息
+      userInfo: {
+        username: "未知用户",
+        avatar:
+          "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+      },
+      // 保存消息中心的有关信息
+      messageInfo: {
+        newCommentsCount: 0,
+        newAnswersCount: 0,
+        newFollowersCount: 0,
+      },
+      isLogin: true, // 标识用户是否登录
+      articles: [], // 存放文章列表
     };
   },
   created() {
     this.getUserInfo();
-    this.pullNewestArticles();
+    this.getNewestArticles();
+    this.getMessageCenter();
   },
   methods: {
     handleSelect(key, keyPath) {
@@ -94,9 +140,10 @@ export default {
       });
     },
     // 获取最新的文章列表
-    pullNewestArticles() {
-      this.getPageArticles(1);
+    getNewestArticles() {
+      this.getPageArticles(1); // 获取第一页最新的文章
     },
+    // 按页获取文章
     getPageArticles(pageIndex) {
       this.$http({
         url: this.$http.adornUrl("http://zhizhi.com/blog/questions/get/all"),
@@ -109,6 +156,10 @@ export default {
           this.$message.error(data.msg);
         }
       });
+    },
+    // 获取新的评论数、回复数、关注数
+    getMessageCenter() {
+      // 发送获取内容的请求
     },
   },
 };
